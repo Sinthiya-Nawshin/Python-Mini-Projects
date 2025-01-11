a909238ff5b1b0e11e5df7b462e1bff0ae5d3d54
Sinthiya Nawshin <39787823+Sinthiya-Nawshin@users.noreply.github.com>
2025-01-11 14:05:03 +0600

    Update python_pizza.py

--- python_pizza.py
+++ python_pizza.py
@@ -1,19 +1,19 @@
+# Calculating a pizza shop bill. Depending on the size and toppings choice, the price gets added to the final bill.
+
 print("Welcome to Python Pizza Shop!")
 
 size = input("What size pizza do you want? S, M, or L? ")
 pepperoni = input("Do you want pepperoni as topping? Y or N? ")
 cheese = input("Do you want cheese on it? Y or N? ")
+
 bill = 0
 
 if size == "S":
     bill += 15
-
 elif size == "M":
     bill += 20
-
 elif size == "L":
     bill += 25
-
 else:
     print("Sorry, not a valid size.")
 
